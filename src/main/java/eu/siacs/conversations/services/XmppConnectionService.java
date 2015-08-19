thedevstack/PiratX

commit 855c2cfd0ad069a67ddb8005ef1a447724ab3584

provide extra interface to close sockets. fixes #1330

src/main/java/eu/siacs/conversations/services/XmppConnectionService.java
@@ -280,9 +280,8 @@ public class XmppConnectionService extends Service implements OnPhoneContactsLoa
 
 				scheduleWakeUpCall(Config.PING_MAX_INTERVAL, account.getUuid().hashCode());
 			} else if (account.getStatus() == Account.State.OFFLINE) {
-				resetSendingToWaiting(account);
 				if (!account.isOptionSet(Account.OPTION_DISABLED)) {
-					int timeToReconnect = mRandom.nextInt(50) + 10;
+					int timeToReconnect = mRandom.nextInt(20) + 10;
 					scheduleWakeUpCall(timeToReconnect,account.getUuid().hashCode());
 				}
 			} else if (account.getStatus() == Account.State.REGISTRATION_SUCCESSFUL) {

src/main/java/eu/siacs/conversations/persistance/FileBackend.java
@@ -22,6 +22,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.Socket;
 import java.net.URL;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
@@ -553,6 +554,15 @@ public class FileBackend {
 		if (stream != null) {
 			try {
 				stream.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
+	public static void close(Socket socket) {
+		if (socket != null) {
+			try {
+				socket.close();
 			} catch (IOException e) {
 			}
 		}

commit a775452581ed05c24dbefd6e519ee71fda6d9dae

fixed nasty 'stuck at sending' bug that got introduced in 855c2cfd0ad069a67ddb8005ef1a447724ab3584

src/main/java/eu/siacs/conversations/services/XmppConnectionService.java
@@ -280,6 +280,7 @@ public class XmppConnectionService extends Service implements OnPhoneContactsLoa
 
 				scheduleWakeUpCall(Config.PING_MAX_INTERVAL, account.getUuid().hashCode());
 			} else if (account.getStatus() == Account.State.OFFLINE) {
+				resetSendingToWaiting(account);
 				if (!account.isOptionSet(Account.OPTION_DISABLED)) {
 					int timeToReconnect = mRandom.nextInt(20) + 10;
 					scheduleWakeUpCall(timeToReconnect,account.getUuid().hashCode());