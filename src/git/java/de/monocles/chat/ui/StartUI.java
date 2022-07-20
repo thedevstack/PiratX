d64efab75fdb87043ae37fbba4b5215de0d7414b
Arne <arne@monocles.de>
2022-07-20 20:00:04 +0200

    code cleanup

--- src/git/java/de/monocles/chat/ui/StartUI.java
+++ src/git/java/de/monocles/chat/ui/StartUI.java
@@ -1,28 +1,20 @@
 package de.monocles.chat.ui;
-import android.Manifest;
+
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
-import androidx.appcompat.app.AlertDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.List;
+
 import eu.siacs.conversations.Config;
 import eu.siacs.conversations.R;
 import eu.siacs.conversations.ui.ConversationsActivity;
 import eu.siacs.conversations.ui.util.IntroHelper;
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.EasyPermissions;
 public class StartUI extends AppCompatActivity {
-    private static final int NeededPermissions = 1000;
-    String[] perms = {Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +23,7 @@ public class StartUI extends AppCompatActivity {
         String PREF_FIRST_START = "FirstStart";
         SharedPreferences FirstStart = getApplicationContext().getSharedPreferences(PREF_FIRST_START, Context.MODE_PRIVATE);
         long FirstStartTime = FirstStart.getLong(PREF_FIRST_START, 0);
-        Log.d(Config.LOGTAG, "All permissions granted, starting " + getString(R.string.app_name) + "(" + FirstStartTime + ")");
+        Log.d(Config.LOGTAG, "Permissions granted or not, Starting " + getString(R.string.app_name) + "(" + FirstStartTime + ")");
         Intent intent = new Intent(this, ConversationsActivity.class);
         intent.putExtra(PREF_FIRST_START, FirstStartTime);
         startActivity(intent);
